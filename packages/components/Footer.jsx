a8ef240cc6e7662147d0a513d1b937a38e379b79
Alexsander Souza <92753962+parvilucifera@users.noreply.github.com>
2022-10-21 19:34:21 -0300

    fix: footer and card padding

--- packages/components/Footer.jsx
+++ packages/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className="sticky bottom-0 text-center mt-8 bg-gray-800 text-red-200 p-2  w-full">
+    <div className="relative bottom-0 text-center mt-8 bg-gray-800 text-red-200 p-2  w-full">
       Niseko Resorts <br />
       +81(0)123 45 6789 | contact@contact.com <br />
       HTM Technincal Test by Alexsander Souza
